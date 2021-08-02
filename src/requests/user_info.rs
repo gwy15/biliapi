57736d4438221e3afec58163cbb971bd28ef27d5
gwy15 <gwy15thu@gmail.com>
2021-08-02 21:54:01 +0800

    fix user info

--- src/requests/user_info.rs
+++ src/requests/user_info.rs
@@ -3,16 +3,18 @@ use crate::requests::prelude::*;
 /// 用户信息，返回如粉丝数、头像等信息
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct UserInfo {
-    pub mid: u64,
+    // pub mid: u64,
     pub name: String,
     /// 头像链接
     #[serde(rename = "face")]
     pub avatar_url: String,
 
     /// 粉丝数
+    #[serde(rename = "fans")]
     pub followers: u64,
 
     /// 关注数
+    #[serde(rename = "attention")]
     pub following: u64,
 
     /// 个性签名
@@ -27,6 +29,22 @@ impl Request for UserInfo {
     fn request(client: &Client, args: Self::Args) -> RequestResponse<Self> {
         const URL: &str = "https://api.bilibili.com/x/web-interface/card";
         let r = client.get(URL).query(&[("mid", args)]).send();
-        Box::pin(async move { r.await?.bili_data().await })
+
+        #[derive(Debug, Deserialize)]
+        struct Outer {
+            card: UserInfo,
+        }
+
+        Box::pin(async move {
+            let outer: Outer = r.await?.bili_data().await?;
+            Ok(outer.card)
+        })
     }
+}
+
+#[tokio::test]
+async fn test_user_info() {
+    let client = crate::connection::new_client().unwrap();
+    let info = UserInfo::request(&client, 672328094).await.unwrap();
+    assert_eq!(info.name, "嘉然今天吃什么");
 }
