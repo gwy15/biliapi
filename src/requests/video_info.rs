f1c02605559d3ebc30b420949f301af9ea4696ba
gwy15 <gwy15thu@gmail.com>
2021-08-04 23:02:13 +0800

    有的时候 ctime 也没有

--- src/requests/video_info.rs
+++ src/requests/video_info.rs
@@ -29,8 +29,8 @@ pub struct VideoInfo {
     pub publish_at: DateTime<Utc>,
 
     /// 用户投稿时间
-    #[serde(rename = "ctime", with = "chrono::serde::ts_seconds")]
-    pub create_at: DateTime<Utc>,
+    #[serde(rename = "ctime", with = "chrono::serde::ts_seconds_option", default)]
+    pub create_at: Option<DateTime<Utc>>,
 
     pub desc: String,
 
@@ -91,4 +91,64 @@ async fn test_video_info() -> Result<()> {
     assert!(info.title.contains("亲爱的那不是爱情"));
 
     Ok(())
+}
+
+#[test]
+fn test_video_info_parse_from_tag() {
+    let s = r#"
+    {
+        "aid": 759539804,
+        "videos": 1,
+        "tid": 31,
+        "tname": "翻唱",
+        "copyright": 2,
+        "pic": "http://i1.hdslb.com/bfs/archive/c951667bccc7dd4ddbe425f83888e8a5553ce11e.jpg",
+        "title": "A-SOUL/珈乐】《历历万乡》",
+        "pubdate": 1628088921,
+        "desc": "https://live.bilibili.com/22634198/\nhttps://live.bilibili.com/22634198/\n珈乐 2021-8-4 直播剪辑\n\n\n\n点个关注呗~\nA-SOUL主页链接：https://space.bilibili.com/703007996/ \n向晚：https://space.bilibili.com/672346917\n乃琳：https://space.bilibili.com/672342685\n珈乐：https://space.bilibili.",
+        "state": 0,
+        "duration": 216,
+        "rights": {
+            "bp": 0,
+            "elec": 0,
+            "download": 0,
+            "movie": 0,
+            "pay": 0,
+            "hd5": 0,
+            "no_reprint": 0,
+            "autoplay": 1,
+            "ugc_pay": 0,
+            "is_cooperation": 0,
+            "ugc_pay_preview": 0,
+            "no_background": 0
+        },
+        "owner": {
+            "mid": 19409621,
+            "name": "Bahumt",
+            "face": "http://i2.hdslb.com/bfs/face/19d6aeaf72e4914c46aa29fbfd90906aa656f378.jpg"
+        },
+        "stat": {
+            "aid": 759539804,
+            "view": 1,
+            "danmaku": 0,
+            "reply": 0,
+            "favorite": 0,
+            "coin": 0,
+            "share": 0,
+            "now_rank": 0,
+            "his_rank": 0,
+            "like": 0,
+            "dislike": 0
+        },
+        "dynamic": "",
+        "cid": 382633251,
+        "dimension": {
+            "width": 1920,
+            "height": 1080,
+            "rotate": 0
+        },
+        "short_link_v2": "https://b23.tv/BV1b64y1B79o",
+        "bvid": "BV1b64y1B79o"
+    }"#;
+    serde_json::from_str::<VideoInfo>(s).unwrap();
 }
